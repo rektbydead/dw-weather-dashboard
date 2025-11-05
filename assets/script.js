666a42ab9c518f165685a8f68702eb5733a14566
RektByDead <rlousada123456@gmail.com>
2025-11-05 09:38:09 +0000

    rename css variables to be more specific

--- assets/script.js
+++ assets/script.js
@@ -81,19 +81,19 @@ async function setLocation(location) {
 function set5DayForecast(forecast) {
 	const dailyForecasts = forecast.DailyForecasts
 	document.getElementById("daily-forecast").innerHTML = dailyForecasts.map((forecast) => `
-		<div class="hourly-forecast-item">
-			<span class="hourly-forecast-item-day"> ${dateToWeekDay(forecast.Date)} </span>
+		<div class="daily-forecast-item">
+			<span class="daily-forecast-item-day"> ${dateToWeekDay(forecast.Date)} </span>
 			
-			<div class="hourly-forecast-item-image">
+			<div class="daily-forecast-item-image">
 				<img 
 					src="https://www.accuweather.com/assets/images/weather-icons/v2a/${forecast.Day.Icon}.svg" 
-					alt="hourly-forecast-item-${forecast.Day.Icon}"
+					alt="daily-forecast-item-${forecast.Day.Icon}"
 				/>
 				
 				 <span> ${forecast.Day.IconPhrase} </span>
 			</div>
 			
-			<div class="hourly-forecast-item-temperature">
+			<div class="daily-forecast-item-temperature">
 				<span class="maximum">
 					${fahrenheitToCelsius(forecast.Temperature.Maximum.Value)}°
 				</span> 
