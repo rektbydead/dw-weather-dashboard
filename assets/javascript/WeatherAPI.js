8c4939e39a8b141da5c2db034e91671d714336ac
Ruben Lousada <ruben.lousada.dev@gmail.com>
2025-11-06 00:00:52 +0000

    redo WeatherApi to cache every request

--- assets/javascript/WeatherAPI.js
+++ assets/javascript/WeatherAPI.js
@@ -1,68 +1,45 @@
 const ACCUWEATHER_API_KEY = "zpka_760a7f024433458ea1b84c1b33211150_924d685f"
+const CACHE_TIMEOUT = 1800 * 1000 // 30 minutes
 
-const defaultLocationKey = "349727"
+async function cachedFetch(url, options) {
+	const now = Date.now();
+	const cached = localStorage.get(url);
 
-async function getDefaultLocation() {
-	const defaultLocationString = localStorage.getItem(`default-location`)
-	if (defaultLocationString) {
-		return JSON.parse(defaultLocationString)
+	if (cached && now - cached.timestamp < CACHE_TIMEOUT) {
+		return cached.data
 	}
 
-	const response = await fetch(`https://dataservice.accuweather.com/locations/v1/${defaultLocationKey}/`,{
-		headers: {
-			Authorization: `Bearer ${ACCUWEATHER_API_KEY}`,
-		},
-	})
-
-	const location = await response.json()
-	localStorage.setItem(`default-location`, JSON.stringify(location))
-	return location
-}
-
-async function getLocationByKey(locationKey) {
-	const response = await fetch(`https://dataservice.accuweather.com/locations/v1/${locationKey}/`,{
-		headers: {
-			Authorization: `Bearer ${ACCUWEATHER_API_KEY}`,
-		},
-	})
-
-	return response.json()
-}
-
-async function lookupLocation(locationName) {
-	const savedLocationString = localStorage.getItem(`location-lookup-${locationName}`)
-	if (savedLocationString) {
-		return JSON.parse(savedLocationString)
-	}
-
-	const response = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?q=${locationName}`,{
+	const response = await fetch(url, {
 		headers: {
 			Authorization: `Bearer ${ACCUWEATHER_API_KEY}`,
 		},
 	})
 
 	const data = await response.json()
-	const splicedData = data.splice(0, 3)
-	localStorage.setItem(`location-lookup-${locationName}`, JSON.stringify(splicedData))
-	return splicedData
+
+	localStorage.setItem(url, { data, timestamp: now })
+	return data;
+}
+
+
+const defaultLocationKey = "349727"
+
+async function getDefaultLocation() {
+	const url = `https://dataservice.accuweather.com/locations/v1/${defaultLocationKey}/`
+	return cachedFetch(url)
+}
+
+async function lookupLocation(locationName) {
+	const url =`https://dataservice.accuweather.com/locations/v1/cities/search?q=${locationName}`
+	return cachedFetch(url)
 }
 
 async function get5DayForecast(locationKey) {
-	const response = await fetch(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`,{
-		headers: {
-			Authorization: `Bearer ${ACCUWEATHER_API_KEY}`,
-		},
-	})
-
-	return await response.json()
+	const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`
+	return cachedFetch(url)
 }
 
 async function getTodayForecast(locationKey) {
-	const response = await fetch(`https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${locationKey}`,{
-		headers: {
-			Authorization: `Bearer ${ACCUWEATHER_API_KEY}`,
-		},
-	})
-
-	return await response.json()
+	const url = `https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${locationKey}`
+	return cachedFetch(url)
 }
